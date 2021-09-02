8e1bcae37d9f68564a84e66edb6a2ea96ccd3940
Leonienutz <89557133+Leonienutz@users.noreply.github.com>
2021-09-02 11:52:24 -0500

    Create script.js

---
+++ Source Code/js/script.js
@@ -0,0 +1,22 @@
+// import d3 lib here
+
+
+/*
+move this part into seperate js file 
+(name it user input for example)
+start
+*/
+
+// get todays date and display as default for date choice
+// var today = new Date();
+// month values range from 0-11, so we add 1 to get the result
+// var date_today = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
+// document.getElementById("user_date").innerHTML = date_today;
+
+//let latitude_input_group = document.querySelector('.latitude_input_group');
+//let latitude_input = document.querySelector('#latitude');
+
+
+/*
+end
+*/
