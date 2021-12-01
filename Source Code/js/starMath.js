44b02c1668bd2b19858a7d696701246b939e30bb
gkashaa <73720369+gkashaa@users.noreply.github.com>
2021-12-01 12:22:44 -0600

    Update starMath.js

--- Source Code/js/starMath.js
+++ Source Code/js/starMath.js
@@ -1,30 +1,47 @@
-
-
 class StarMath{
-
-
-
-StarMath(RA, dec, LST){
-this.RA = RA;
-this.dec = dec;
-var LST;
-var e;
-var A;
-}
-
-function azimuthElevation(){
-    var H;
-    var lat;
-    var lon;
+    
+    // Parameters will be changed to lat, long, date, and time
+    constructor(lat, long, LST){
+    this.lat = lat;
+    this.long = long;
+    this.LST = LST;
     var e;
     var A;
+    }
 
-    H = LST - RA;
-    A = Math.atan(-(Math.sin(H)Math.cos(dec))/(Math.cos(lat)Math.sin(dec) - Math.sin(lat)Math.cos(dec)Math.cos(H)));
-    e = Math.asin(Math.sin(lat)Math.sin(dec) + Math.cos(lat)Math.cos(dec)Math.cos(H));
+    // Function to convert date and time -> GMT
+    // Function to convert GMT -> LST
+   
+   
+    // Function to conver righ acension and declination to azimuth and elevation
+    azimuthElevation(RA, decl){
 
-    this.e = e;
-    this.A = A;
+        var H = this.LST - RA;
+        var A = atan((-1)*(sin(H) * cos(decl)) / (cos(this.lat) * sin(decl) - sin(this.lat) * cos(decl) * cos(H)));
+        var e = asin(sin(this.lat) * sin(decl) + cos(this.lat) * cos(decl) * cos(H));
+        A = (A * 180)/ Math.PI;
+        e = (e * 180)/ Math.PI;
+        return [A, e];
+        // returns elevation then azimuth in an array.
+    }
 }
 
+
+
+// Functions to convert trig functions to degrees
+
+function sin(x) {
+    return (Math.sin(x * (Math.PI / 180)));
+}
+
+function cos(x) {
+    return (Math.cos(x * (Math.PI / 180)));
+}
+
+function atan(x) {
+    return (Math.atan(x) * (180 / Math.PI));
+}
+
+function asin(x) {
+    return (Math.asin(x) * (180 / Math.PI));
 }
