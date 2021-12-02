c48e9f36de35dd20d94f92530c26c03db28ae99e
gkashaa <73720369+gkashaa@users.noreply.github.com>
2021-12-02 12:53:24 -0600

    Update map_projection.js

--- Source Code/js/map_projection.js
+++ Source Code/js/map_projection.js
@@ -3,8 +3,7 @@ var star = new StarMath(-6.7924, 39.2083, 22.0984235779);
 
 // Dimensions of the projection
 var width = 5000;       
-var height = 5000;      
-
+var height = 5000; 
 
 // Create and configure stereographic projection
 var projection = d3.geoStereographic()
@@ -19,25 +18,35 @@ var path = d3.geoPath(projection);
 var graticule = d3.geoGraticule()
     .stepMinor([45, 10])();
 
+// Outline for 0
+var outline = d3.geoCircle().radius(90).center([0, 90])();
+
 // Create an SVG element (for the body)
 var svg = d3.select("body")
     .append("svg")
     .attr("viewBox", [0, 0, width, height])
     .attr("fill", "currentColor")
-    .style("background", "black")
+    .style("background", "#1f1f1f")
     .style("margin", "-10");
-
-// Add another SVG element (for the graticule)
-svg.append("path")
+  
+// Add an SVG element (for the graticule)
+svg.append("path") 
     .attr("d", path(graticule))
     .attr("fill", "none")
     .attr("stroke", "white")
     .attr("stroke-opacity", 1);
 
+// Add an SVG element (for the outline)
+svg.append("path")
+    .attr("d", path(outline))
+    .attr("fill", "none")
+    .attr("stroke", "yellow");
+
 
 
 d3.csv("hyg.csv", function(error, data) {
     if (error) throw error;
+
 
     
     // Add the stars to the map projection
@@ -55,6 +64,7 @@ d3.csv("hyg.csv", function(error, data) {
                              
 		.attr("r", "2px")  // Magnitude of the stars (not done)
 		.attr("fill", "white");
+                
                 
     
         
@@ -84,51 +94,56 @@ d3.csv("hyg.csv", function(error, data) {
     
   
   
-	d3.json("constellations.json", function(error, cons) {
-	    if (error) throw error;     
+           
+     
+   
+d3.json("constellations.json", function(error, cons) {
+    if (error) throw error;     
+
+        
+    // Create constellations 
+    var pLine = d3.line()
+       .x(function(p) { return (projection(star.azimuthElevation(p.ra, p.dec))[0]); })
+       .y(function(p) { return (projection(star.azimuthElevation(p.ra, p.dec))[1]); })
+       .defined(function (p) { return (p.new_line) !== null; });
 
 
-	    // Create constellations 
-	    var pLine = d3.line()
-	       .x(function(p) { if (p.new_line !== null) return (projection(star.azimuthElevation(p.ra, p.dec))[0]); })
-	       .y(function(p) { if (p.new_line !== null) return (projection(star.azimuthElevation(p.ra, p.dec))[1]); })
-	       .defined(function (p) { return (p.new_line) !== null; });
+    // Add the constellations
+    svg.append("path")
+        .data(cons)
+        .attr("d", pLine(cons))
+        .attr("fill", "none")
+        .attr("stroke", "#8ae8ff")
+        .attr("stroke-width", "2")     // Hides the constellations
+        .attr("opacity", 0);    
 
 
-	    // Add the constellations
-	    svg.append("path")
-		.data(cons)
-		.attr("d", pLine(cons))
-		.attr("fill", "none")
-		.attr("stroke", "#8ae8ff")
-		.attr("stroke-width", "2")
-		.attr("opacity", 0);        // Hides the constellations
+    // Add the constellation names
+    var con_name = svg.selectAll("#figure_text")
+        .data(cons)
+        .enter()
+        .append("text")
+        .attr("x", function(p) { if (p.new_line !== null) return ((projection(star.azimuthElevation(p.ra, p.dec))[0]) + 30); })
+        .attr("y", function(p) { if (p.new_line !== null) return ((projection(star.azimuthElevation(p.ra, p.dec))[1])); })
+        .attr("fill", "#fffb8a")
+        .text(function(p) { 
+                    return (p.con); });
 
 
-	    // Add the constellation names
-	    var con_name = svg.selectAll("#figure_text")
-		.data(cons)
-		.enter()
-		.append("text")
-		.attr("x", function(p) { if (p.new_line !== null) return ((projection(star.azimuthElevation(p.ra, p.dec))[0]) + 30); })
-		.attr("y", function(p) { if (p.new_line !== null) return ((projection(star.azimuthElevation(p.ra, p.dec))[1])); })
-		.attr("fill", "#fffb8a")
-		.text(function(p) { 
-			    return (p.con); });
+    // Checkbox to show/hide constellation labels
+    var checkbox_2 = document.getElementById('constellationLabel');     
 
 
-	    // Checkbox to show/hide constellation labels
-	    var checkbox_2 = document.getElementById('constellationLabel');     
+    // Checkbox listener for the constellation label checkbox
+    checkbox_2.addEventListener('change', (event) => {      
+        if (event.currentTarget.checked) {
+          con_name.attr("opacity", 0);        // Hide labels if checkbox is checked                      
+        } else {
+          con_name.attr("opacity", 1);       // Show labels if checkbox is unchecked 
+        }
+      });
 
 
-	    // Checkbox listener for the constellation label checkbox
-	    checkbox_2.addEventListener('change', (event) => {      
-		if (event.currentTarget.checked) {
-		  con_name.attr("opacity", 0);        // Hide labels if checkbox is checked                      
-		} else {
-		  con_name.attr("opacity", 1);       // Show labels if checkbox is unchecked 
-		}
-	      });
 
-    	});
+    });
 }); 
