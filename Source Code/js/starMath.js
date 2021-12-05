d3e48e697b7e23bcaa79062db16d439acb3a4fd5
gkashaa <73720369+gkashaa@users.noreply.github.com>
2021-12-05 02:09:09 -0600

    Update starMath.js

--- Source Code/js/starMath.js
+++ Source Code/js/starMath.js
@@ -17,10 +17,12 @@ class StarMath{
     azimuthElevation(RA, decl){
 
         var H = this.LST - RA;
-        var A = atan((-1)*(sin(H) * cos(decl)) / (cos(this.lat) * sin(decl) - sin(this.lat) * cos(decl) * cos(H)));
-        var e = asin(sin(this.lat) * sin(decl) + cos(this.lat) * cos(decl) * cos(H));
-        A = (A * 180)/ Math.PI;
-        e = (e * 180)/ Math.PI;
+        var A = Math.atan((-1)*(Math.sin(H) * Math.cos(decl)) / (Math.cos(this.lat) * Math.sin(decl)) - (Math.sin(this.lat) * Math.cos(decl) * Math.cos(H)));
+        var e = Math.asin((Math.sin(this.lat) * Math.sin(decl)) + (Math.cos(this.lat) * Math.cos(decl) * Math.cos(H)));
+        A = (A * 180) / Math.PI;
+        e = (e * 180) / Math.PI;
+
+        console.log([A, e]);
         return [A, e];
         // returns elevation then azimuth in an array.
     }
@@ -44,22 +46,4 @@ function magnitude (mag) {
             n--;
         }
         return (mag);
-}
-
-
-// Functions to convert trig functions to degrees
-function sin(x) {
-    return (Math.sin(x * (Math.PI / 180)));
-}
-
-function cos(x) {
-    return (Math.cos(x * (Math.PI / 180)));
-}
-
-function atan(x) {
-    return (Math.atan(x) * (180 / Math.PI));
-}
-
-function asin(x) {
-    return (Math.asin(x) * (180 / Math.PI));
 }
